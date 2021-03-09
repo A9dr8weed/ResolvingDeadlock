e7ef7e2795410c93e5fa3124af7ca6a6155f6007
Andrew <andsof123455@gmail.com>
2021-03-09 14:06:54 +0200

    Добавьте файлы проекта.

---
+++ ResolvingDeadlock/Account.cs
@@ -0,0 +1,47 @@
+﻿namespace ResolvingDeadlock
+{
+    /// <summary>
+    /// Account.
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Account balance.
+        /// </summary>
+        private double _balance;
+
+        /// <summary>
+        /// Account ID.
+        /// </summary>
+        private readonly int _id;
+
+        public Account(int id, double balance)
+        {
+            _id = id;
+            _balance = balance;
+        }
+
+        /// <summary>
+        /// Public property.
+        /// </summary>
+        public int ID => _id;
+
+        /// <summary>
+        /// Withdraw money.
+        /// </summary>
+        /// <param name="amount"> Amount to withdraw. </param>
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+
+        /// <summary>
+        /// Deposit money.
+        /// </summary>
+        /// <param name="amount"> Amount to deposit. </param>
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+    }
+}
---
+++ ResolvingDeadlock/AccountManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace ResolvingDeadlock
+{
+    /// <summary>
+    /// Account manager.
+    /// </summary>
+    public class AccountManager
+    {
+        /// <summary>
+        /// From account.
+        /// </summary>
+        private readonly Account _fromAccount;
+
+        /// <summary>
+        /// To account.
+        /// </summary>
+        private readonly Account _toAccount;
+
+        /// <summary>
+        /// Amount to transfer.
+        /// </summary>
+        private readonly double _amountToTransfer;
+
+        public AccountManager(Account fromAccount, Account toAccount, double amountToTransfer)
+        {
+            _fromAccount = fromAccount;
+            _toAccount = toAccount;
+            _amountToTransfer = amountToTransfer;
+        }
+
+        /// <summary>
+        /// Transfer method.
+        /// </summary>
+        public void Transfer()
+        {
+            // Object to lock.
+            object _lock1, _lock2;
+
+            // If fromAccount is < toAccount.
+            if (_fromAccount.ID < _toAccount.ID)
+            {
+                _lock1 = _fromAccount;
+                _lock2 = _toAccount;
+            }
+            else
+            {
+                _lock1 = _toAccount;
+                _lock2 = _fromAccount;
+            }
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {((Account)_lock1).ID}");
+
+            lock (_lock1)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {((Account)_lock1).ID}");
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} suspended for 1 second");
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} back in action and trying to acquire lock on {((Account)_lock2).ID}");
+
+                lock (_lock2)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {((Account)_lock2).ID}");
+
+                    _fromAccount.Withdraw(_amountToTransfer);
+                    _toAccount.Deposit(_amountToTransfer);
+
+                    Console.WriteLine($"{Thread.CurrentThread.Name} Transfered {_amountToTransfer} from {_fromAccount.ID} to {_toAccount.ID}");
+                }
+            }
+        }
+    }
+}
